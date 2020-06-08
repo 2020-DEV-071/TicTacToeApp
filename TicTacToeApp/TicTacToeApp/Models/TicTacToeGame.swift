78c437f86a3f5719e18b9b002c157cbacc1270b7
2020-DEV-071 <d.rajamanikanta@icloud.com>
2020-06-08 21:24:08 +0100

    Test to place O first throws error

--- TicTacToeApp/TicTacToeApp/Models/TicTacToeGame.swift
+++ TicTacToeApp/TicTacToeApp/Models/TicTacToeGame.swift
@@ -1,4 +1,13 @@
 
+enum GameError: Error, Equatable {
+    
+    case playerXShouldMoveFirst(message: String)
+}
+
+enum GameConstants {
+    
+    static let invalidFirstPlayer = "X should always goes first"
+}
 
 struct TicTacToeGame {
     
@@ -16,6 +25,13 @@ struct TicTacToeGame {
     }
     
     mutating func place(player: Player, at position: Position) throws {
+        
+        if self.gameBoard.unfilledSquares == 9  {
+            guard player == .x else {
+                throw GameError.playerXShouldMoveFirst(message: GameConstants.invalidFirstPlayer)
+            }
+        }
+        
         self.gameBoard.board[position.row][position.coloumn] = player
     }
     
--- TicTacToeApp/TicTacToeAppTests/TicTacToeGameTests.swift
+++ TicTacToeApp/TicTacToeAppTests/TicTacToeGameTests.swift
@@ -40,11 +40,11 @@ class TicTacToeGameTests: XCTestCase {
         XCTAssertEqual(unfilledSquares, 9)
     }
     
-    func test_placeXFirst_throwsNoError() {
+    func test_xPlayerGoesFirst_throwsNoError() {
         
         let playerX = game.firstPlayer!
-        
         let row0Coloumn0 = Position(row: 0, coloumn: 0)
+        
         XCTAssertNoThrow(try self.game.place(player: playerX, at: row0Coloumn0))
     }
     
@@ -53,6 +53,7 @@ class TicTacToeGameTests: XCTestCase {
         let row0Coloumn0 = Position(row: 0, coloumn: 0)
         try! self.game.place(player: .x, at: row0Coloumn0)
         let unfilledSqaures = self.game.gameBoard.unfilledSquares
+        
         XCTAssertEqual(unfilledSqaures, 8)
     }
     
@@ -63,5 +64,14 @@ class TicTacToeGameTests: XCTestCase {
         let player = self.game.gameBoard.player(at: row0Coloumn0)
         
         XCTAssertEqual(player, Player.x)
+    }
+    
+    func test_oPlayerGoesFirst_throwsError() {
+        
+        let row0Coloumn0 = Position(row: 0, coloumn: 0)
+        
+        XCTAssertThrowsError(try self.game.place(player: .o, at: row0Coloumn0)) { error in
+            XCTAssertEqual(error as! GameError, GameError.playerXShouldMoveFirst(message: "X should always goes first"))
+        }
     }
 }
