0d8d04e58501565a4f2dffb4001426f190708250
2020-DEV-071 <d.rajamanikanta@icloud.com>
2020-06-11 14:10:36 +0100

    ViewController tests to select and reset items in collection view

--- TicTacToeApp/TicTacToeAppTests/ViewControllerTests.swift
+++ TicTacToeApp/TicTacToeAppTests/ViewControllerTests.swift
@@ -47,10 +47,8 @@ class ViewControllerTests: XCTestCase {
     
     func test_cellForItemAtIndexPath_returnsCell() {
         
-        let indexPath = IndexPath(row: 1, section: 0)
-        
         let cell2 = self.gameVC.collectionView(self.gameVC.collectionView,
-                                               cellForItemAt: indexPath)
+                                               cellForItemAt: IndexPaths.r0c0)
         
         XCTAssertNotNil(cell2)
     }
@@ -77,28 +75,66 @@ class ViewControllerTests: XCTestCase {
     
     func test_selectCollectionViewAllItems_setStatusDraw() {
         
-        let collectionView = self.gameVC.collectionView!
+        let drawCellItems = [IndexPaths.r0c0,
+                             IndexPaths.r1c0,
+                             IndexPaths.r2c0,
+                             IndexPaths.r0c1,
+                             IndexPaths.r0c2,
+                             IndexPaths.r1c1,
+                             IndexPaths.r1c2,
+                             IndexPaths.r2c2,
+                             IndexPaths.r2c1]
         
-        let drawIndexPaths = [IndexPaths.r0c0,
-                              IndexPaths.r1c0,
-                              IndexPaths.r2c0,
-                              IndexPaths.r0c1,
-                              IndexPaths.r0c2,
-                              IndexPaths.r1c1,
-                              IndexPaths.r1c2,
-                              IndexPaths.r2c2,
-                              IndexPaths.r2c1]
-        
-        drawIndexPaths.forEach {
-            collectionView.selectItem(at: $0,
-                                      animated: false,
-                                      scrollPosition: .centeredVertically)
-            
-            self.gameVC.collectionView(collectionView,
-                                       didSelectItemAt: $0)
-        }
+        self.selectItems(at: drawCellItems)
         
         XCTAssertEqual(self.gameVC.statusLabel.text!, "Game Draw")
+    }
+    
+    func test_selectCollectionViewItemsToWin_setStatusWin() {
+        
+        let leftDiagonalCellItems = [IndexPaths.r0c0,
+                                     IndexPaths.r0c1,
+                                     IndexPaths.r1c1,
+                                     IndexPaths.r0c2,
+                                     IndexPaths.r2c2]
+        
+        self.selectItems(at: leftDiagonalCellItems)
+        
+        XCTAssertEqual(self.gameVC.statusLabel.text!, "Win!! x")
+    }
+    
+    func test_selectCollectionViewSameItemTwice_setStatusError() {
+        
+        let sameCellItems = [IndexPaths.r0c0,
+                             IndexPaths.r0c0]
+        
+        self.selectItems(at: sameCellItems)
+        
+        XCTAssertEqual(self.gameVC.statusLabel.text!, "Position already played")
+    }
+    
+    func test_resetGame_clearsAllItems() {
+        
+        let firstTwoItems = [IndexPaths.r0c0, IndexPaths.r0c1]
+        self.selectItems(at: firstTwoItems)
+        self.gameVC.resetGame(UIButton())
+        
+        let allItems = [IndexPaths.r0c0,
+                        IndexPaths.r1c0,
+                        IndexPaths.r2c0,
+                        IndexPaths.r0c1,
+                        IndexPaths.r0c2,
+                        IndexPaths.r1c1,
+                        IndexPaths.r1c2,
+                        IndexPaths.r2c2,
+                        IndexPaths.r2c1]
+        
+        for indexItem in allItems {
+            
+            let cell = self.gameVC.collectionView(self.gameVC.collectionView,
+                                                  cellForItemAt: indexItem)
+            XCTAssertEqual(cell.contentView.backgroundColor, UIColor.gray)
+        }
     }
 }
 
@@ -109,5 +145,19 @@ extension ViewControllerTests {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         self.gameVC = storyboard.instantiateViewController(identifier: "ViewController")
         self.gameVC.loadViewIfNeeded()
+    }
+    
+    private func selectItems(at indexPaths: [IndexPath]) {
+        
+        let collectionView = self.gameVC.collectionView!
+        
+        indexPaths.forEach {
+            collectionView.selectItem(at: $0,
+                                      animated: false,
+                                      scrollPosition: .centeredVertically)
+            
+            self.gameVC.collectionView(collectionView,
+                                       didSelectItemAt: $0)
+        }
     }
 }
